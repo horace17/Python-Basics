ae7affa6772643804607b8d38598f30e74e8c7c4
horace <hocedrix2010@gmail.com>
2024-10-24 13:46:58 +0300

    python

---
+++ operators.py
@@ -0,0 +1,21 @@
+# print(25+25)
+
+math = 90
+eng = 80
+kis = 70
+sci = 80
+
+var1 = 90
+var2 = 80
+
+product = var1 * var2
+
+remainder = var1/var2
+
+print(remainder)
+
+# print(product)
+
+# total = math+eng+kis+sci
+
+# print(total)
---
+++ variables.py
@@ -0,0 +1,11 @@
+name = "John"
+school = 2
+list = ["John",1,"Slim","*"]
+average = 89.23
+in_school = True
+
+# print(list)
+
+# formatted format
+
+print(f"My name is {name}")
