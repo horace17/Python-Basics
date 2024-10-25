dab54a5760d1674dc5e87b7479221351131dbcbb
horace <hocedrix2010@gmail.com>
2024-10-25 13:12:37 +0300

    python data types

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+laptop = {
+    "brand": "hp",
+    "Processor": "4GHz",
+    "price": 1000
+
+}
+
+# key value pairs
+# print(laptop["brand"])
+# print(len(laptop))
+
+student = {
+    "name": "Slim",
+    "year": 2,
+    "semester": 2,
+    "units": ["Maths", "Physics", "Kiswahili", "Science"],
+    "school_fee": 20000
+}
+
+print(student)
